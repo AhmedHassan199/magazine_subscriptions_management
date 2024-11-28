a0255d9290bad790e8e40f7547fff74adcab5953
Ahmed Mohamed <amohamed@uittai.com>
2024-11-28 13:37:17 +0200

    fix

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Article extends BaseModel
 {
     use HasFactory;
+    use LogsActivity;
 
     // Define the fillable fields for the Article model
     protected $fillable = [
